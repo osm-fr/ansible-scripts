8ad025babdf80ede8394ee44312566e835eac2cf
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2016-06-11 18:50:52 +0200

    Add cron for letsencrypt, to regenerate certificate every two months

--- roles/letsencrypt/files/launch-acme-tiny.sh
+++ roles/letsencrypt/files/launch-acme-tiny.sh
@@ -5,3 +5,13 @@ set -e
 python ../acme-tiny/acme_tiny.py --account-key ./account.key --csr ./domain.csr --acme-dir /data/project/letsencrypt/challenges > ./signed.crt.tmp
 
 mv ./signed.crt.tmp ./signed.crt
+
+if [ -e intermediate.pem ]; then
+  cat ./signed.crt intermediate.pem > chained.pem
+  if [ -e /etc/init.d/apache2 ]; then
+    sudo /etc/init.d/apache2 reload
+  fi
+  if [ -e /etc/init.d/nginx ]; then
+    sudo /etc/init.d/nginx reload
+  fi
+fi
