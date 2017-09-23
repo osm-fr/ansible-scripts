22723997925eb5e31efe28b8ca859c786f7c20dc
Rodolphe Pelloux-Prayer <rodolphe@damsy.net>
2017-09-23 15:13:50 +0200

    chore: use ansible.cfg instead of environment var

--- init.fish
+++
@@ -1,3 +0,0 @@
-set -xg ANSIBLE_HOSTS (pwd)/hosts
-set -xg ANSIBLE_INVENTORY (pwd)/hosts
-set -xg ANSIBLE_TRANSPORT "ssh"
--- init.sh
+++
@@ -1,3 +0,0 @@
-export ANSIBLE_HOSTS=$(pwd)/hosts
-export ANSIBLE_INVENTORY=$(pwd)/hosts
-export ANSIBLE_TRANSPORT="ssh"
