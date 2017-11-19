8ec708b2068515175e3fa2f5d8267935aea1175d
Jocelyn Jaubert <jocelyn.jaubert@gmail.com>
2017-11-19 19:45:55 +0100

    letsencrypt: Run launch-acme-tiny.sh every week by checking age of signed.crt before regenerating it

--- roles/letsencrypt/files/launch-acme-tiny.sh
+++ roles/letsencrypt/files/launch-acme-tiny.sh
@@ -1,6 +1,21 @@
-#! /bin/sh
+#! /bin/bash
 
 set -e
+
+need_update=0
+
+if [ ! -f "./signed.crt" ]; then
+  need_update=1
+elif [ "$(find ./account.key ./domain.csr -newer ./signed.crt)" ]; then
+  need_update=1
+elif [ "$(find ./signed.crt -mtime +62)" ]; then
+  # file is more than 2 months old
+  need_update=1
+fi
+
+if [ "$need_update" -eq 0 ]; then
+  exit 0
+fi
 
 python ../acme-tiny/acme_tiny.py --account-key ./account.key --csr ./domain.csr --acme-dir /data/project/letsencrypt/challenges > ./signed.crt.tmp
 
